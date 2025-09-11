2193b9e9d6e5c737891059ed34e9cc5b0c284a26
Tim Crossley <tim.crossley@echonous.com>
2025-09-11 16:46:44 -0700

    Add dependency on ai-edge-torch converter

---
+++ src/echonous/exporters/ai_edge_exporter.py
@@ -0,0 +1,23 @@
+import ai_edge_torch
+import torch
+
+from echonous.models import Model
+
+
+def sample_inputs(model: Model):
+    return tuple(
+        torch.rand(io.shape) for io in model.inputs
+    )
+
+def main():
+    from echonous.models.loaders import load_model
+    model = load_model('guidance.psax_av')
+    inputs = sample_inputs(model)
+    print(inputs)
+    tflite_model = ai_edge_torch.convert(model.module, inputs)
+    print(tflite_model)
+    tflite_model.export('guidance_psax_av.tflite')
+    .
+
+if __name__ == '__main__':
+    main()
